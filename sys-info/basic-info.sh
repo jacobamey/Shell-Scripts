b230445eb6b619704bc38883908d7bc3213c10a7
Jacob Amey <jacobamey@gmail.com>
2013-08-03 12:22:31 -0400

    Update basic-info.sh

--- sys-info/basic-info.sh
+++ sys-info/basic-info.sh
@@ -7,11 +7,11 @@
 # Last Updated: July 9th 2013
 ##################################################
 # 
-echo "Info about the server:" > /store/docs/Info.txt
-echo "##############################" >> /store/docs/Info.txt
-uname -a >> /store/docs/Info.txt
-echo "##############################" >> /store/docs/Info.txt
-cat /etc/sysconfig/network-scripts/ifcfg-eth0 >> /store/docs/Info.txt
-echo "##############################" >> /store/docs/Info.txt
-route >> /store/docs/Info.txt
-echo "##############################" >> /store/docs/Info.txt
+echo "Info about the server:" > /blah/docs/Info.txt
+echo "##############################" >> /blah/docs/Info.txt
+uname -a >> /blah/docs/Info.txt
+echo "##############################" >> /blah/docs/Info.txt
+cat /etc/sysconfig/network-scripts/ifcfg-eth0 >> /blah/docs/Info.txt
+echo "##############################" >> /blah/docs/Info.txt
+route >> /blah/docs/Info.txt
+echo "##############################" >> /blah/docs/Info.txt
