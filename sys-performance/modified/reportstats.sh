aeef0736ee57aefcb0537956269b565fba35c7a0
Jacob Amey <jamey@securityinspection.com>
2013-10-15 09:26:53 -0400

    Added html strict tag to help with IE compatibility.

--- sys-performance/modified/reportstats.sh
+++ sys-performance/modified/reportstats.sh
@@ -15,6 +15,8 @@ HOSTNAME=`hostname`
 IP=`/sbin/ifconfig eth0 | grep "inet addr" | gawk -F: '{print $2}' | gawk '{print $1}'`
 ###########################################################
 # Create Head and CSS Presentation
+
+echo "<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">"
 echo "<html>" > $TEMP_FILE
 echo "<head>" >> $TEMP_FILE
 echo "<style>" >> $TEMP_FILE
@@ -143,4 +145,3 @@ echo "</tbody></table></body></html>" >> $TEMP_FILE
 rm -f $TEMP_FILE
 rm -f $REPORT_FILE
 # EOF
-
